532a050c3a7f406f6777de4967ba6398009c5f8b
Maligan <maligan@rambler.ru>
2012-01-10 20:25:52 +0400

    Powerfull changes :-) I don't remember all
    Popup: Colored names, Mouse whell. All include in one place
    ToolTip for macro

--- Binding_Extension.lua
+++ Binding_Extension.lua
@@ -1,5 +1,12 @@
 BE = {}
 
+-- Constants
+local BE_PROMPT_BINDING = "Назначьте клавишу для %s" 
+local BE_CREATE_BINDING = "Назначить "..NORMAL_FONT_COLOR_CODE.."%s"..FONT_COLOR_CODE_CLOSE.." для %s" 
+local BE_CLEAR_BINDING = "Сбросить клавишу для %s"
+-- Why it does not exist?
+local FONT_COLOR_CODE_OPEN = "|c"
+
 -- Switch on/off binding text in ToolTip 
 function BE:EnableTip(bool)
 	if bool and not self.TipHooked then 
@@ -30,6 +37,21 @@ function BE:HookTip()
 		end
 	end)
 
+	-- Macro (There is not OnTooltipSetMacro)
+	GameTooltip:HookScript("OnShow", function(self)
+		action = self:GetOwner().action
+		if action ~= nil then
+			actionType, id, subType = GetActionInfo(action)
+			if actionType == "macro" then
+				local name = GetMacroInfo(id) 
+				local key = GetBindingKey("MACRO "..name) 
+				BE.AddTip(self, key)
+				-- For resize
+				self:Show()
+			end
+		end
+	end)
+
 	-- Note about hooking 
 	self.TipHooked = true
 end 
@@ -37,31 +59,45 @@ end
 -- Append keybind text to first tooltip line
 function BE.AddTip(toolTip, bindingKey)
 	if bindingKey then
-		keyTip = NORMAL_FONT_COLOR_CODE.."("..bindingKey..")"..FONT_COLOR_CODE_CLOSE 
+		keyTip = NORMAL_FONT_COLOR_CODE.."("..BE.GetLocalizedKey(bindingKey)..")"..FONT_COLOR_CODE_CLOSE 
 		local nameLine = _G[toolTip:GetName().."TextLeft1"]
 		nameLine:SetText(nameLine:GetText().." "..keyTip)
 	end
 end
 
--- Static Popup initialization 
+-- Static Popup definition 
 StaticPopupDialogs["BINDING_EXTENSION"] = {
+	text = "",
 	button1 = TEXT(ACCEPT), 
 	button2 = TEXT(CANCEL), 
 	timeout = 0,
 	whileDead = true,
 	hideOnEscape = false,
-	-- Save origin script hsndlers
-	OnShow = function (self, ...)
-		self.data = {}
+	-- Initialize dialog
+	OnShow = function (self, ...) 
+		self.data.info = StaticPopupDialogs["BINDING_EXTENSION"]
+		self.data.colorizeEntity = BE.GetColored(self.data.kind, self.data.entity)
+		-- New or Owerride?
+		local key = GetBindingKey(self.data.kind.." "..self.data.entity)
+		if key == nil then
+			self.text:SetFormattedText(BE_PROMPT_BINDING, self.data.colorizeEntity)
+			self.button1:Disable()
+		else
+			self.text:SetFormattedText(BE_CLEAR_BINDING, self.data.colorizeEntity)
+		end	
+		-- Change script handlers
 		self.data.OriginOnKeyDown = self:GetScript("OnKeyDown")
 		self.data.OriginOnMouseDown = self:GetScript("OnMouseDown")
-		self:SetScript("OnKeyDown", BE.KeyboardHandler)
-		self:SetScript("OnMouseDown", BE.MouseHandler)
+		self.data.OriginOnMouseWheel = self:GetScript("OnMouseDown")
+		self:SetScript("OnKeyDown", self.data.info.keyboardHandler)
+		self:SetScript("OnMouseDown", self.data.info.mouseHandler)
+		self:SetScript("OnMouseWheel", self.data.info.wheelHandler)
 	end,
 	-- Return origin script handlers
 	OnHide = function (self, ...)
 		self:SetScript("OnKeyDown", self.data.OriginOnKeyDown)
 		self:SetScript("OnMouseDown", self.data.OriginOnMouseDown)
+		self:SetScript("OnMouseWheel", self.data.OriginOnMouseWheel)
 		self.data = nil
 	end,
 	-- Disable accept button in combat
@@ -69,13 +105,15 @@ StaticPopupDialogs["BINDING_EXTENSION"] = {
 		if self.data.combat ~= UnitAffectingCombat("player") then
 			self.data.combat = UnitAffectingCombat("player")
 			if self.data.combat then
-				self.button1:Disable();
+				self.button1:Disable()
 				self:SetScript("OnKeyDown", self.data.OriginOnKeyDown)
 				self:SetScript("OnMouseDown", self.data.OriginOnMouseDown)
+				self:SetScript("OnMouseWheel", self.data.OriginOnMouseWheel)
 			else
-				self.button1:Enable();
-				self:SetScript("OnKeyDown", BE.KeyboardHandler)
-				self:SetScript("OnMouseDown", BE.MouseHandler)
+				self.button1:Enable()
+				self:SetScript("OnKeyDown", self.data.info.keyboardHandler)
+				self:SetScript("OnMouseDown", self.data.info.mouseHandler)
+				self:SetScript("OnMouseWheel", self.data.info.wheelHandler)
 			end
 		end
 	end,
@@ -83,8 +121,113 @@ StaticPopupDialogs["BINDING_EXTENSION"] = {
 	OnAccept = function (self, ...)
 		BE.SafelySetBinding(self.data.key, self.data.kind.." "..self.data.entity)
 	end,
+	-- Handle keyboard input
+	keyboardHandler = function(dialog, key)
+		if key == "UNKNOWN" 
+			or string.find(key, "SHIFT") 
+			or string.find(key, "CTRL") 
+			or string.find(key, "ALT") then return end
+	
+		key = BE.GetModifiers()..key
+		dialog.data.info.change(dialog, key)
+	end,	
+	-- Handle mouse input
+	mouseHandler = function(dialog, key)
+		if key == "UNKNOWN" then return
+		elseif key == "LeftButton" then key = "BUTTON1" 
+		elseif key == "RightButton" then key = "BUTTON2"
+		elseif key == "MiddleButton" then key = "BUTTON3"
+		else key = key:upper() end
+
+		key = BE.GetModifiers()..key
+		dialog.data.info.change(dialog, key)
+	end,
+	wheelHandler = function(dialog, arg)
+		local key
+		if arg > 0 then 
+			key = "MOUSEWHEELUP"
+		else 
+			key = "MOUSEWHEELDOWN" 
+		end
+		key = BE.GetModifiers()..key
+		dialog.data.info.change(dialog, key)
+	end,
+	-- Change input key
+	change = function(dialog, key)
+		if key ~= nil then
+			dialog.data.key = key 
+			-- Change text
+			local localizedKey = BE.GetLocalizedKey(key)
+			local coloredName = BE.GetColored(dialog.data.kind,dialog.data.entity)
+			_G[dialog:GetName().."Text"]:SetFormattedText(BE_CREATE_BINDING, localizedKey, coloredName)
+			dialog.button1:Enable()
+			-- Resizing
+			dialog.maxWidthSoFar = nil 
+			dialog.maxHeightSoFar = nil 
+			StaticPopup_Resize(dialog, "BINDING_EXTENSION")
+		end
+	end
 }
 
+-- Code sugar :-)
+function BE.PopupDialog(kind, entity) StaticPopup_Show("BINDING_EXTENSION", nil, nil, { kind = kind, entity = entity }) end
+
+function BE.IsModifiedPress() return IsShiftKeyDown() and IsControlKeyDown() and IsAltKeyDown() end
+
+function BE.GetModifiers()
+	local mods = "" 
+	if IsShiftKeyDown() then mods = "SHIFT-" end
+	if IsControlKeyDown() then mods = "CTRL-"..mods end
+	if IsAltKeyDown() then mods = "ALT-"..mods end
+	return mods
+end
+
+-- Safely set binding
+function BE.SafelySetBinding(key, command)
+	local oldKey = GetBindingKey(command)
+	-- If there is not change
+	if oldKey == key then return; end
+	-- Clear key for binding
+	if oldKey ~= nil then SetBinding(oldKey); end
+	-- Create binding, else return binding
+	if key ~= nil and not SetBinding(key, command) and oldKey then SetBinding(oldKey, command) end
+	-- Save current binding set
+	SaveBindings(GetCurrentBindingSet())
+end
+
+-- Nice view of item/spell/macro
+function BE.GetColored(kind, entity)
+	-- Default color 
+	local color = "FFFFD200"
+	-- Switch
+	if kind == "ITEM" then
+		_, _, quality = GetItemInfo(entity)
+		_, _, _, color = GetItemQualityColor(quality)
+	elseif kind == "SPELL" then
+		color = "FF71D5FF"
+	end	
+	-- Combine
+	return FONT_COLOR_CODE_OPEN..color.."["..entity.."]"..FONT_COLOR_CODE_CLOSE
+end
+
+-- Localized name of key
+function BE.GetLocalizedKey(key) 
+	-- Parsing
+	local modKeys = ""
+	local dashIndex = key:reverse():find("-")
+	if dashIndex then
+		modKeys = key:sub(1, -dashIndex)
+		key = key:sub(-dashIndex+1)
+	end
+	-- Localization
+	if IsMacClient() then suffix = "_MAC" else suffix = "" end
+	localized = _G["KEY_"..key..suffix]
+	if not localized then localized = key end
+	-- Result
+	return modKeys..localized 
+end
+
+-- Attach popup to different game button
 -- Attach to Spell button
 SpellButton_OriginOnModifiedClick = SpellButton_OnModifiedClick 	
 SpellButton_OnModifiedClick = function (self, button)
@@ -92,109 +235,34 @@ SpellButton_OnModifiedClick = function (self, button)
 		local slot = SpellBook_GetSpellBookSlot(self)
 		local spellName = GetSpellBookItemName(slot, SpellBookFrame.bookType)
 		if spellName and not IsPassiveSpell(slot, SpellBookFrame.bookType) then
-			BE:Popup("SPELL", spellName) 
+			BE.PopupDialog("SPELL", spellName) 
 		end
 	else
 		SpellButton_OriginOnModifiedClick(self, button)
 	end
 end
 
--- Attach to Container button 
+-- Attach to Container button (Bag item)
 ContainerFrameItemButton_OriginOnModifiedClick = ContainerFrameItemButton_OnModifiedClick 	
 ContainerFrameItemButton_OnModifiedClick = function (self, button)
 	if BE.IsModifiedPress() and button == "LeftButton" then 
 		local itemName = GetItemInfo(GetContainerItemID(self:GetParent():GetID(), self:GetID()))
-		if itemName ~= nil and GetItemSpell(itemName) ~= null then
-			BE:Popup("ITEM", itemName) 
+		if itemName ~= nil then
+			BE.PopupDialog("ITEM", itemName) 
 		end
 	else
 		ContainerFrameItemButton_OriginOnModifiedClick(self, button)
 	end
 end
 
--- Attach to Macro
-MacroFrame_SelectMacro = function (...)
-	print "Select"
-end
-
-
---button:HookScript("OnEnter", function () BindingExtension.UpdateCursor() end)
---button:HookScript("OnEnter", function () BindingExtension.UpdateCursor() end)
---button:HookScript("OnKeyUp", function () BindingExtension.UpdateCursor() end)
---button:HookScript("OnLeave", function () ResetCursor() end)
-
-function BE.IsModifiedPress() return IsShiftKeyDown() and IsControlKeyDown() and IsAltKeyDown() end
-
-BE_BIND_TEXT = "Назначить "..NORMAL_FONT_COLOR_CODE.."%s"..FONT_COLOR_CODE_CLOSE.." для |cff71d5ff[%s]|r" 
-BE_PROMPT_TEXT = "Назначьте клавишу для |cff71d5ff%s|r" 
-BE_CLEAR_TEXT = "Сбросить клавишу для |cff71d5ff%s|r"
-
-
-function BE.KeyboardHandler(popup, key)
-	if key == "UNKNOWN" 
-		or string.find(key, "SHIFT") 
-		or string.find(key, "CTRL") 
-		or string.find(key, "ALT") then return nil; end
-	key = BE.AddModifiers(key)
-	if key ~= nil then
-		popup.data.key = key
-		_G[popup:GetName().."Text"]:SetText(format(BE_BIND_TEXT, popup.data.key, popup.data.entity))
+-- Attach to Macro button
+MacroButton_OriginOnClick = MacroButton_OnClick 	
+MacroButton_OnClick  = function (self, button)
+	MacroButton_OriginOnClick(self, button)
+	if BE.IsModifiedPress() and button == "LeftButton" then 
+		local name = _G[self:GetName().."Name"]:GetText()
+		BE.PopupDialog("MACRO", name)
 	end
 end
-
-function BE:Popup(kind, entity)
-	-- Override binding or create new?
-	key = GetBindingKey(kind.." "..entity)
-	if key == nil then StaticPopupDialogs["BINDING_EXTENSION"].text = format(BE_PROMPT_TEXT, "["..entity.."]") 
-	else StaticPopupDialogs["BINDING_EXTENSION"].text = format(BE_CLEAR_TEXT, "["..entity.."]") end
-	-- Show standart popup
-	local frame = StaticPopup_Show("BINDING_EXTENSION")
-	frame.data.kind = kind
-	frame.data.entity = entity
-end
-
-function BE.KeyTransform(key)
-	-- Non-bindable keys
-	if key == "PRINTSCREEN" then Screenshot(); return; end
-	--[[
-	local Map = {
-		"LeftButton" = "BUTTON1",
-		"RightButton" = "BUTTON2",
-		"MiddleButton" = "BUTTON3",
-		"Button4" = "BUTTON4",
-		"Button5" = "BUTTON5"
-	}]]
-end
-
-function BE.AddModifiers(key)
-	if ( IsShiftKeyDown() ) then key = "SHIFT-"..key; end
-	if ( IsControlKeyDown() ) then key = "CTRL-"..key; end
-	if ( IsAltKeyDown() ) then key = "ALT-"..key; end
-	return key;
-end
-
-function BE.SafelySetBinding(key, command)
-	local oldKey = GetBindingKey(command)
-	-- Reset old binding
-	if oldKey ~= nil then SetBinding(oldKey); end
-	-- Create binding, else return binding
-	if key ~= nil and not SetBinding(key, command) and oldKey then
-		SetBinding(oldKey, command)
-	end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 BE:EnableTip(true)
