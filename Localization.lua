95bba509454e970abba9245675ace08273ef2049
Maligan <maligan@rambler.ru>
2012-01-24 18:17:45 +0400

    Add localization for RU, EN, FR, GE

--- Localization.lua
+++ Localization.lua
@@ -1,3 +1,29 @@
-BINDING_EXTENSION_PROMPT_BINDING = "Назначьте клавишу для %s" 
-BINDING_EXTENSION_CREATE_BINDING = "Назначить "..NORMAL_FONT_COLOR_CODE.."%s"..FONT_COLOR_CODE_CLOSE.." для %s" 
-BINDING_EXTENSION_CLEAR_BINDING = "Сбросить клавишу для %s"
+﻿local prompt, create, clear 
+local locale = GetLocale()
+
+if locale == "ruRU" then
+	-- [Russian] mother tongue first ;-)
+	prompt = "Назначьте клавишу для %s" 
+	create = "Назначить %s для %s" 
+	clear = "Сбросить клавишу для %s"
+elseif locale == "frFR" then
+	-- [French]
+	prompt = "Créer raccourci clavier pour %s"
+	create = "Créer %s pour %s"
+	clear = "Effacer raccourci clavier pour %s"
+elseif locale == "deDE" then
+	-- [German] I studied German in 5th class! Hellow Germany!
+	prompt = "Satz Tastenkombination für %s"
+	create = "Satz %s für %s"
+	clear = "Klarer Tastenkombination für %s"
+else
+	-- [English] is default
+	prompt = "Set keybinding for %s" 
+	create = "Set %s for %s" 
+	clear = "Clear keybinding for %s"
+end
+
+-- Apply Locale
+BINDING_EXTENSION_PROMPT_BINDING = prompt 
+BINDING_EXTENSION_CREATE_BINDING = create
+BINDING_EXTENSION_CLEAR_BINDING = clear
