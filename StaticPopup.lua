1b4305fae81a447984e0f1f354e3cabfd874b7d4
Alexandr Frolov <maligan@rambler.ru>
2019-09-10 03:25:36 +0300

    Fix for WoW:Classic

--- StaticPopup.lua
+++ StaticPopup.lua
@@ -20,21 +20,19 @@ StaticPopupDialogs["BINDING_EXTENSION"] = {
 	timeout = 0,
 	whileDead = true,
 	hideOnEscape = false,
+
 	--
 	-- StaticPopup standard handlers
 	--
 	-- Initialize dialog
-	OnShow = function (self, ...) 
+	OnShow = function (self, ...)
 		self.data.info = StaticPopupDialogs["BINDING_EXTENSION"]
 		self.data.colorizedName = self.data.info.GetColored(self.data.kind, self.data.name)
+
 		-- New or Override?
 		local key = GetBindingKey(self.data.kind.." "..self.data.name)
-		if key == nil then
-			self.text:SetFormattedText(BINDING_EXTENSION_PROMPT_BINDING, self.data.colorizedName)
-			self.button1:Disable()
-		else
-			self.text:SetFormattedText(BINDING_EXTENSION_CLEAR_BINDING, self.data.colorizedName)
-		end	
+		self.data.info.Refresh(self, key, key ~= nil)
+
 		-- Change script handlers
 		self.data.OriginOnKeyDown = self:GetScript("OnKeyDown")
 		self.data.OriginOnMouseDown = self:GetScript("OnMouseDown")
@@ -52,8 +50,9 @@ StaticPopupDialogs["BINDING_EXTENSION"] = {
 	end,
 	-- Disable accept button in combat
 	OnUpdate = function (self, ...)
-		if self.data.combat ~= UnitAffectingCombat("player") then
-			self.data.combat = UnitAffectingCombat("player")
+		combat = UnitAffectingCombat("player")
+		if self.data.combat ~= combat then
+			self.data.combat = combat
 			if self.data.combat then
 				self.button1:Disable()
 				self:SetScript("OnKeyDown", self.data.OriginOnKeyDown)
@@ -69,57 +68,64 @@ StaticPopupDialogs["BINDING_EXTENSION"] = {
 	end,
 	-- Create binding
 	OnAccept = function (self, ...)
-		self.data.info.SafelySetBinding(self.data.key, self.data.kind.." "..self.data.name)
+		local command = self.data.kind.." "..self.data.name
+
+		if self.data.clear then
+			self.data.info.SafelySetBinding(nil, command)
+		else
+			self.data.info.SafelySetBinding(self.data.key, command)
+		end
 	end,
 	-- 
 	-- Input handlers
 	--
 	-- Handle keyboard input
-	KeyboardHandler = function (dialog, key)
+	KeyboardHandler = function (self, key)
 		if key == "UNKNOWN" 
 			or string.find(key, "SHIFT") 
 			or string.find(key, "CTRL") 
 			or string.find(key, "ALT") then return end
 	
-		key = dialog.data.info.GetModifiers()..key
-		dialog.data.info.Change(dialog, key)
+		self.data.info.Refresh(self, self.data.info.GetModifiers()..key, false)
 	end,	
 	-- Handle mouse input
-	MouseHandler = function (dialog, key)
+	MouseHandler = function (self, key)
 		if key == "UNKNOWN" then return
 		elseif key == "LeftButton" then key = "BUTTON1" 
 		elseif key == "RightButton" then key = "BUTTON2"
 		elseif key == "MiddleButton" then key = "BUTTON3"
 		else key = key:upper() end
 
-		key = dialog.data.info.GetModifiers()..key
-		dialog.data.info.Change(dialog, key)
+		self.data.info.Refresh(self.data.info.GetModifiers()..key, false)
 	end,
 	-- Handle mouse wheel
-	WheelHandler = function (dialog, arg)
+	WheelHandler = function (self, arg)
 		local key
-		if arg > 0 then 
-			key = "MOUSEWHEELUP"
-		else 
-			key = "MOUSEWHEELDOWN" 
-		end
-		key = dialog.data.info.GetModifiers()..key
-		dialog.data.info.Change(dialog, key)
+		if arg > 0 then key = "MOUSEWHEELUP" else key = "MOUSEWHEELDOWN" end
+		self.data.info.Refresh(self.data.info.GetModifiers()..key, false)
 	end,
 	-- Change status key
-	Change = function (dialog, key)
-		if key ~= nil then
-			dialog.data.key = key 
-			-- Change text
+	Refresh = function (self, key, clear)
+		self.data.key = key
+		self.data.clear = clear;
+
+		-- Change text & button
+		if clear == true then
+		 	self.text:SetFormattedText(BINDING_EXTENSION_CLEAR_BINDING, self.data.colorizedName)
+			self.button1:Enable()
+		elseif key == nil then
+			self.text:SetFormattedText(BINDING_EXTENSION_PROMPT_BINDING, self.data.colorizedName)
+			self.button1:Disable()
+		else
 			local localizedKey = NORMAL_FONT_COLOR_CODE..GetBindingText(key, "KEY_")..FONT_COLOR_CODE_CLOSE
-			local colorizedName = dialog.data.colorizedName
-			_G[dialog:GetName().."Text"]:SetFormattedText(BINDING_EXTENSION_CREATE_BINDING, localizedKey, colorizedName)
-			if not dialog.button1:IsEnabled() then dialog.button1:Enable() end
-			-- Resizing
-			dialog.maxWidthSoFar = nil 
-			dialog.maxHeightSoFar = nil 
-			StaticPopup_Resize(dialog, "BINDING_EXTENSION")
+			self.text:SetFormattedText(BINDING_EXTENSION_CREATE_BINDING, localizedKey, self.data.colorizedName)
+			self.button1:Enable()
 		end
+
+		-- Resizing
+		self.maxWidthSoFar = nil 
+		self.maxHeightSoFar = nil 
+		StaticPopup_Resize(self, "BINDING_EXTENSION")
 	end,
 	--
 	-- Misc
@@ -130,14 +136,21 @@ StaticPopupDialogs["BINDING_EXTENSION"] = {
 		-- If there is not change
 		if oldKey == key then return; end
 		-- Clear old binding
-		if oldKey ~= nil then SetBinding(oldKey); end
-		-- Create binding 
+		if oldKey ~= nil then SetBinding(oldKey) end
+		-- Create binding
 		if key ~= nil and not SetBinding(key, command) and oldKey ~= nil then 
 			-- If error then return old binding
 			SetBinding(oldKey, command)
 		else
 			-- Save current binding set	
-			SaveBindings(GetCurrentBindingSet())
+			local which = GetCurrentBindingSet()
+
+			-- For actual & classic WoW versions this methods are different
+			local flush = nil
+			if SaveBindings ~= nil then flush = SaveBindings
+			elseif AttemptToSaveBindings ~= nil then flush = AttemptToSaveBindings end 
+
+			flush(which)
 		end
 	end,
 	-- Colored view of item/spell/macro
